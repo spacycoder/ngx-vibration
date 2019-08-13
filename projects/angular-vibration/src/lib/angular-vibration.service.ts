b1d1c1a7fdfd87eb1db906e239b0fd83a0d930d2
SpacyCoder <sral_h@hotmail.com>
2019-08-13 22:14:15 +0200

    docs(AngularVibrationService): add tsdoc comments

--- projects/angular-vibration/src/lib/angular-vibration.service.ts
+++ projects/angular-vibration/src/lib/angular-vibration.service.ts
@@ -5,8 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class AngularVibrationService {
 
-  constructor() { }
-
+  /**
+   * Takes vibrationPattern as input and returns a boolean indicating success
+   * @param vibratePattern An array of values describes alternating periods of time in which the device is vibrating and not vibrating.
+   * @returns a boolean indicating whether or not the vibration was successfull.
+   * Will return false if the browser doesn't suppport the vibration API.
+   */
   vibrate(vibratePattern: number[]): boolean {
     const supportsVibration = window.navigator.vibrate;
     if (supportsVibration) {
@@ -16,6 +20,11 @@ export class AngularVibrationService {
     return !!supportsVibration;
   }
 
+  /**
+   * Cancels the currently active vibrations
+   * @returns a boolean indicating whether or not the vibration was successfull.
+   * Will return false if the browser doesn't suppport the vibration API.
+   */
   cancelVibration(): boolean {
     const supportsVibration = window.navigator.vibrate;
     if (supportsVibration) {
@@ -25,7 +34,11 @@ export class AngularVibrationService {
     return !!supportsVibration;
   }
 
+  /**
+   * Checks if the browser has support for the vibration API.
+   * @returns a boolean indicating support for the vibration API.
+   */
   hasVibrationSupport(): boolean {
-    return  !!window.navigator.vibrate;
+    return !!window.navigator.vibrate;
   }
 }
