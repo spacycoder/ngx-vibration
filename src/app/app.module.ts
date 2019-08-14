95df04e0d01b480d876340cd0f73f0d6427dcfe0
SpacyCoder <sral_h@hotmail.com>
2019-08-14 10:57:25 +0200

    feat(angular-vibration-demo): add vibration directive example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularVibrationModule } from 'angular-vibration';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AngularVibrationModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AngularVibrationModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
